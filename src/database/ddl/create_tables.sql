413910a10d639cd9aeb119d7e49fa694e0e80d24
justindekock <jdekock17@gmail.com>
2025-03-13 20:18:11 -0500

    updated ddl

--- src/database/ddl/create_tables.sql
+++ src/database/ddl/create_tables.sql
@@ -1,14 +1,29 @@
 /*CREATING DATABASE TABLES*/
 use prohoops;
+
+create table if not exists season (
+    season_id int not null,
+    season varchar(13), 
+    season_desc varchar(30),
+    primary key (season_id)
+);
+
+create table if not exists game_type (
+    game_type tinyint not null,
+    game_type_desc varchar(50),
+    primary key (game_type)
+);
+
 create table if not exists game (
     game_id int,
     season_id int, 
-    game_date date, /*YYYY-MM-DD*/
-    matchup varchar(12), /*use home team's matchup*/
-    final_score varchar(18), /*LAL 110 - 109 BOS*/
+    game_date date, 
+    matchup varchar(12), 
+    final_score varchar(18), 
     ot_ind tinyint,
     game_type tinyint,
-    primary key (game_id)
+    primary key (game_id),
+    foreign key (season_id) references season (season_id)
 ); 
 
 create table if not exists active_teams (
@@ -29,8 +44,8 @@ create table if not exists active_players (
 create table if not exists team_gamelog (
     game_id int not null,
     team_id int not null, 
-    win_ind tinyint, /*1 is win*/
-    home_ind tinyint, /*1 is home*/
+    win_ind tinyint,
+    home_ind tinyint, 
     primary key (game_id, team_id),
     foreign key (game_id) references game (game_id),
     foreign key (team_id) references active_teams (team_id)
@@ -78,7 +93,7 @@ create table if not exists player_shooting (
 create table if not exists team_box (
     game_id int not null, 
     team_id int not null,
-    mins tinyint, -- mins > 240 indicates overtime
+    mins tinyint,
     pts tinyint, 
     ast tinyint, 
     reb tinyint, 
@@ -105,23 +120,6 @@ create table if not exists team_shooting (
     fgp decimal(10,2),
     fg3p decimal(10,2),
     ftp decimal(10,2),
-    primary key (game_id, team_id)
+    primary key (game_id, team_id),
+    foreign key (team_id) references active_teams (team_id)
 );
-
-create table if not exists game_type (
-    game_type tinyint not null,
-    game_type_desc varchar(50),
-    primary key (game_type)
-);
-
-
-
-
-
-/*insert into game_type values (1, 'Preseason'), (2, 'Regular Season'), (4, 'Playoffs') ;*/
-/*
-use prohoops;
-drop table team_gamelog;
-drop table game;
-*/
-

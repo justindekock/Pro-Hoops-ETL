d15a8145d896698241f0bdef2f1f0bfb2f5f8d96
justindekock <jdekock17@gmail.com>
2025-03-22 16:27:45 -0500

    little changes

--- src/main.py
+++ src/main.py
@@ -3,12 +3,8 @@ from log_config.logs import start_logging, get_logger, log_start_msg, log_close_
 from gmail.gmail import start_gmail_log, send_summary
 
 # PASS DAYS=1, DAYS_BACK=1 FOR THE NIGHTLY SCRIPT
-# TO RUN THE FUNCTION FOR SEVERAL DAYS, PASS NUMBER OF DAYS DESIRED TO DAYS
-# TO START ON A DAY OTHER THAN YESTERDAY, PASS >0 TO DAYS_BACK
-# DAYS=10, DAYS_BACK=5 WOULD START AT 5 DAYS AGO AND FETCH FOR 10 DAYS GOING BACK 
 
 def main():
-    # initiate the logger
     start_logging()
     gmail_log = start_gmail_log()
     
@@ -18,12 +14,9 @@ def main():
     # this calls all functions in other modules - running this fetches, cleans, and inserts all data
     fetch_insert_glogs_pbp(gmail_log, days=1, days_back=1)
     
-    
-    
     log_close_msg(logger)
-    # gmail_log_path = '/home/jdeto/programming/nba_etl/nba_current/src/gmail/logs/'
-    # print(get_recent_log(gmail_log_path))
     
+    # sends txt summary via gmail
     send_summary()
     
 if __name__=='__main__':
